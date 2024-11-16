c652759ea28bb051db1eb6c9caff701e3950bc55
pierre gourinat <pierregourinat@gmail.com>
2024-11-16 20:05:50 +0100

    fix montant et style

--- argentbank/src/components/BankAccount.js
+++ argentbank/src/components/BankAccount.js
@@ -1,17 +1,17 @@
 const accountList = [
   {
     title: "Argent Bank Checking (x8349)",
-    amount: "$2,082.79",
+    amount: 2082.79,
     description: "Available Balance",
   },
   {
     title: "Argent Bank Checking (x3948)",
-    amount: "$6,584.21",
+    amount: 6584.21,
     description: "Available Balance",
   },
   {
     title: "Argent Bank Checking (x4398)",
-    amount: "$1,352.57",
+    amount: 1352,
     description: "Available Balance",
   },
 ];
@@ -21,7 +21,7 @@ const BankAccounts = () => {
     <section key={account.title} className="account">
       <div className="account-content-wrapper">
         <h3 className="account-title"> {account.title} </h3>
-        <p className="account-amount"> {account.amount} </p>
+        <p className="account-amount"> ${account.amount} </p>
         <p className="account-amount-description"> {account.description} </p>
       </div>
       <div className="account-content-wrapper cta">
